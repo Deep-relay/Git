130c1b4a6fad9de17605498bf7aab8c66ddeb884
Deep-relay <twitter.mail.gandhi@gmail.com>
2024-01-22 16:35:52 +0530

    Attempt 1 at integrating mongoose

--- new-node-project/New_Node_Test_File.js
+++ new-node-project/New_Node_Test_File.js
@@ -45,6 +45,8 @@
 // });
 
 //yaha se upar 3rd part ke 'a' part ke examples kre hue hain
+const Person=require('./mongo')
+require('dotenv').config()
 let cors=require('cors')
 let express = require("express");
 let morgan = require("morgan");
@@ -79,35 +81,35 @@ app.use(
     })
 );
 let date_obj = new Date();
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
-let port = 3001;
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456",
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523",
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345",
+//     },
+//     {
+//         id: 4,
+//         name: "Mary Poppendieck",
+//         number: "39-23-6423122",
+//     },
+// ];
+let port = process.env.port;
 app.get("/api", (req, res) => {
     res.send("Hi, you are on the Start page of the persons api");
 });
 app.get("/api/persons", (req, res) => {
     // console.log(persons)
-    res.json(persons);
+    Person.find({}).then(persons=>res.json(persons))
 });
 app.get("/api/info", (req, res) => {
     let date = date_obj.getTime();
---
+++ new-node-project/mongo.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose')
+require(dotenv).config()
+const url=process.env.mongo_url
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+const personSchema=new mongoose.Schema({
+    name:String,
+    number:Number,
+})
+personSchema.set('toJSON',{
+    transform:(doc,ret)=>{
+        ret.id=ret._id.toString()
+        delete ret._id
+        delete ret.__v
+    }
+})
+const Person=mongoose.model('Person',personSchema)
+module.exports(Person)
